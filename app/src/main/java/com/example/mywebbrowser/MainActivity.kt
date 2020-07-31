8ff403e1aede96cc6fa954fc190000a2bbb342fa
junminChang <chjm219@kookmin.ac.kr>
2020-08-01 01:11:38 +0900

    onBackPressed & setOnEditorActionListener

--- app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.mywebbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+
+
+        webView.loadUrl("https://www.google.com/")
+
+
+        urlEditView.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                webView.loadUrl(urlEditView.text.toString())
+                true
+            } else {
+                false
+            }
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+
+
     }
 }
