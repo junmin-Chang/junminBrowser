1963d4cd28ffd71e6f6d4dc2b6db73e2545a14ef
junminChang <chjm219@kookmin.ac.kr>
2020-08-01 12:57:35 +0900

    menu

--- app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         webView.apply {
             settings.javaScriptEnabled = true
             webViewClient = WebViewClient()
