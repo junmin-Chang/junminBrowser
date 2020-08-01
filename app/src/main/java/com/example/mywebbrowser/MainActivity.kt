7a1692749437b5f368ecf546421b25f79979971e
junminChang <chjm219@kookmin.ac.kr>
2020-08-01 13:30:31 +0900

    context_menu

--- app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.mywebbrowser
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,6 +37,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+        registerForContextMenu(webView)
     }
 
     override fun onBackPressed() {
@@ -41,5 +49,54 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("https://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("https://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("https://www.daum.net")
+                return true
+            }
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel: 010-7498-1190")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                return true
+
+            }
+            R.id.action_send_text -> {
+                return false
+            }
+            R.id.action_email -> {
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
     }
 }
